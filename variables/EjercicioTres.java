b11a5107f597aa74747fe1cbb5f07df703ec2845
CarlosLmB <corderokrlos1993@gmail.com>
2025-08-15 20:29:03 -0400

    ejercicio tres

---
+++ variables/EjercicioTres.java
@@ -0,0 +1,8 @@
+public class EjercicioTres {
+  public static void main(String[] args) {
+      double precioProducto;
+      precioProducto = 19.99;
+
+      System.out.println("El precio del producto es: " + precioProducto + " USD.");
+  }
+}
